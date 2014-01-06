71b9dbba0fca936993d8a18e34cc64832f73c0a5
choesang <choesang@choesangs-air.home>
2014-01-06 15:33:02 +0100

    removed unwanted Math Class and related unit test added BotSavesPrincess2

---
+++ src/main/java/com/example/codekata/BotSavesPrincess2.java
@@ -0,0 +1,64 @@
+package com.example.codekata;
+
+import java.util.Scanner;
+import java.math.*;
+
+public class BotSavesPrincess2 {
+	
+	/* Head ends here */
+	static void nextMove(int n, int r, int c, String[] grid) {
+		int r1 = -1,c1 = -1;
+		
+		for (int i = 0; i < n; i++) {
+			if (-1 != grid[i].indexOf("p")) {
+				r1 = i;
+				c1 = grid[i].indexOf("p");
+			}
+		}
+		
+		String move = computeMove(r, c, r1, c1);
+//		System.out.println("r1 "+ r1 + " c1 " + c1) ;
+		System.out.println(move);
+		
+	}
+
+	private static String computeMove(int r, int c, int r1, int c1) {
+		
+		int upOrDown = r - r1;
+		int leftOrRight = c - c1;
+		
+		if (Math.abs(upOrDown) > Math.abs(leftOrRight)) {
+			if (upOrDown > 0) {
+				return "UP";
+			} else {
+				return "DOWN";
+			}
+			
+		} else {
+			if (leftOrRight > 0) {
+				return "LEFT";
+			} else {
+				return "RIGHT";
+			}
+			
+		}
+	}
+
+	/* Tail starts here */
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n, r, c;
+		n = in.nextInt();
+		r = in.nextInt();
+		c = in.nextInt();
+		in.useDelimiter("\n");
+		String grid[] = new String[n];
+
+		for (int i = 0; i < n; i++) {
+			grid[i] = in.next();
+		}
+
+		nextMove(n, r, c, grid);
+
+	}
+}
