b16106d316a0b81818f67e5c673f38b25d02cd80
choesang <c.tenzin@secure-shopping.at>
2014-01-22 16:56:21 +0100

    dfs almost complete and partially optimal

--- src/main/java/com/example/codekata/DFS.java
+++ src/main/java/com/example/codekata/DFS.java
@@ -3,7 +3,12 @@
  */
 package com.example.codekata;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.Stack;
 
 /**
  * @author choesang
@@ -36,42 +41,212 @@ public class DFS {
 	private static void dfs(int r, int c, int pacman_r, int pacman_c,
 			int food_r, int food_c, String[] grid) {
 		
-		DFS dfs = new DFS();
-		
 		Node[] adjacencyLists = new Node[r*c];
+		Map<Position, Node> nodeMap = new HashMap<>();
+		
 		int currentNode = 0;
 		for (int i = 0; i < grid.length; i++) {
 			String currentRow = grid[i];
 			for (int j = 0; j < grid[i].length(); j++) {
 				char charValue = currentRow.charAt(j);
 				if (charValue != '%') {
-					adjacencyLists[currentNode] = new Node(i, j, charValue);
+					Node node = new Node(i, j, charValue);
+					adjacencyLists[currentNode] = node;
+					nodeMap.put(node.position, node);
+					initializeNodesNeighbours(node, nodeMap, r, c);
+					
 					++currentNode;
+					
 				}
+			}
+		}
+		
+		Node root = nodeMap.get(new Position(pacman_r, pacman_c));
+		Node food = nodeMap.get(new Position(food_r, food_c));
+		
+//		for (Node node : nodeMap.values()) {
+//			System.out.println(node);
+//		}
+		
+		findByDFS(root, food);
+		
+	}
+
+	private static void initializeNodesNeighbours(Node n, Map<Position, Node> nodeMap, int r, int c) {
+		List<Position> neighbourPositions = getNodesNeighbourPositions(n, r, c);
+		Node neighbour;
+		for(Position p : neighbourPositions) {
+			neighbour = nodeMap.get(p);
+			if(neighbour!= null) {
+				n.addNeighbour(neighbour);
+				neighbour.addNeighbour(n);
 			}
 		}
 	}
 	
-	class Node {
-		Position position;
-		Character value;
-		boolean isVisited = false;
-
-		public Node( int r, int c, Character charValue) {
-			position = new Position(r, c);
-			value = charValue;
-		}
-
-	}
-	
-	class Position {
-		int r, c;
+	private static List<Position> getNodesNeighbourPositions(Node n, int r, int c) {
+		Position above = new Position(n.position.r+1, n.position.c);
+		Position below = new Position(n.position.r-1, n.position.c);
+		Position right = new Position(n.position.r, n.position.c+1);
+		Position left = new Position(n.position.r, n.position.c-1);
 		
-		public Position(int r, int c) {
-			this.r = r;
-			this.c = c;
-		}
+		List<Position> s = new ArrayList<>();
+		
+		if(isValidPosition(above, r, c)) s.add(above);
+		if(isValidPosition(below, r, c)) s.add(below);
+		if(isValidPosition(right, r, c)) s.add(right);
+		if(isValidPosition(left, r, c)) s.add(left);
+		
+		return s;
 	}
 	
+	private static boolean isValidPosition(Position p, int r, int c) {
+		if(p.r < 0 || p.r > r) return false;
+		if(p.c < 0 || p.c > c) return false;
+		return true;
+	}
+	
+	private static void findByDFS(Node root, Node food) {
+		Stack stack = new Stack();
+		stack.push(root);
+		root.isVisited = true;
+		while ( !stack.isEmpty() ) {
+			Node node = (Node) stack.peek();
+			Node child = node.getNextUnvisitedNeighbour();
+			if ( child != null ) {
+				child.isVisited = true;
+				stack.push(child);
+				if (child.value == '.') {
+					break;
+				}
+			} else {
+				stack.pop();
+			}
+		}
+		
+		System.out.println(stack);
+		System.out.println(stack.size());
+		
+	}
 
+}
+
+class Node {
+	Position position;
+	Character value;
+	boolean isVisited = false;
+	Stack<Node> neighbours;
+	
+	public Node getNextUnvisitedNeighbour() {
+		for(Node n : neighbours) {
+			if(!n.isVisited) return n;
+		}
+		return null;
+	}
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((position == null) ? 0 : position.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Node other = (Node) obj;
+		if (position == null) {
+			if (other.position != null)
+				return false;
+		} else if (!position.equals(other.position))
+			return false;
+		return true;
+	}
+
+	public Node( int r, int c, Character charValue) {
+		position = new Position(r, c);
+		value = charValue;
+		neighbours = new Stack<>();
+	}
+
+	public boolean hasNeighbour(Node neighbour) {
+		return neighbours.contains(neighbour);
+	}
+
+	public void addNeighbour(Node node) {
+		if(!hasNeighbour(node)) neighbours.add(node);
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Node [position=" + position + ", value=" + value
+				+ ", isVisited=" + isVisited + ", neighbours.size=" + neighbours.size() + "]";
+	}
+	
+	
+}
+
+class Position {
+	int r, c;
+	
+	public Position(int r, int c) {
+		this.r = r;
+		this.c = c;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + c;
+		result = prime * result + r;
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Position other = (Position) obj;
+		if (c != other.c)
+			return false;
+		if (r != other.r)
+			return false;
+		return true;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Position [r=" + r + ", c=" + c + "]";
+	}
+	
 }
